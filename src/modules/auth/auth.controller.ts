27187892159bdabaae2852516755217afc8e8a62
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2023-08-01 15:09:11 +0200

    added at & rt guards

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { AtGuard } from 'src/guards/at.guard';
 import { TransformDataInterceptor } from 'src/interceptors/transform-data.interceptor';
 import { UserDto } from '../user/dtos/user.dto';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dtos/signin.dto';
 import { SignupDto } from './dtos/signup.dto';
+import { Tokens } from './types/tokens.type';
 
 @Controller('auth')
 export class AuthController {
@@ -16,17 +27,19 @@ export class AuthController {
   }
 
   @Post('/local/signin')
-  signinLocal(@Body() dto: SigninDto) {
+  signinLocal(@Body() dto: SigninDto): Promise<Tokens> {
     return this.authService.signinLocal(dto);
   }
 
+  @UseGuards(AtGuard)
   @Post('/logout')
-  logout() {
-    //return this.authService.logout();
+  logout(@Req() req: Request, userId: number): Promise<boolean> {
+    console.log(req.user['sub']);
+    return this.authService.logout(userId);
   }
 
   @Post('/refresh')
-  refreshTokens() {
-    //return this.authService.refreshTokens();
+  refreshTokens(userId: number, refreshToken: string): Promise<Tokens> {
+    return this.authService.refreshTokens(userId, refreshToken);
   }
 }
